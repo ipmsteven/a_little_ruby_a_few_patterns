49990b3c1173eb2da8b2d7dc4383f4c7a67f3a88
ipmsteven <steven.lyl147@gmail.com>
2018-04-04 00:40:11 -0700

    add RemoveAnchovyFn

--- another_pizza.rb
+++ another_pizza.rb
@@ -1,6 +1,34 @@
-class Pizza; end
+class RemoveAnchovyFn
+  def for_crust
+    Crust.new
+  end
 
-class Crust < Pizza; end
+  def for_topping(topping, pizza)
+    if topping.is_a? Anchovy
+      pizza.remove_anchovy
+    else
+      Topping.new(topping, pizza.remove_anchovy)
+    end
+  end
+end
+
+class Pizza
+  def remove_anchovy
+    raise NotImplementedError
+  end
+
+  private
+
+  def remove_anchovy_fn
+    RemoveAnchovyFn.new
+  end
+end
+
+class Crust < Pizza
+  def remove_anchovy
+    remove_anchovy_fn.for_crust
+  end
+end
 
 class Topping < Pizza
   attr_reader :topping, :pizza
@@ -8,6 +36,10 @@ class Topping < Pizza
   def initialize(topping, pizza)
     @topping = topping
     @pizza   = pizza
+  end
+
+  def remove_anchovy
+    remove_anchovy_fn.for_topping(topping, pizza)
   end
 end
 
@@ -17,4 +49,5 @@ class Salmon < Fish; end
 class Tuna < Fish; end
 
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
+Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_anchovy
 
