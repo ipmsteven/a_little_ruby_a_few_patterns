70b4cd8122cd5daf087a85b62887689ab031132d
ipmsteven <steven.lyl147@gmail.com>
2018-04-06 00:23:17 -0700

    replace remove_anchovy with a more general remove_fish

--- another_pizza.rb
+++ another_pizza.rb
@@ -1,32 +1,32 @@
-class RemoveAnchovyFn
-  def for_crust
+class RemoveFishFn
+  def for_crust(fish)
     Crust.new
   end
 
-  def for_topping(topping, pizza)
-    if topping.is_a? Anchovy
-      pizza.remove_anchovy
+  def for_topping(topping, pizza, fish)
+    if topping.equal? fish
+      pizza.remove_fish(fish)
     else
-      Topping.new(topping, pizza.remove_anchovy)
+      Topping.new(topping, pizza.remove_fish(fish))
     end
   end
 end
 
 class Pizza
-  def remove_anchovy
+  def remove_fish(fish)
     raise NotImplementedError
   end
 
   private
 
-  def remove_anchovy_fn
-    RemoveAnchovyFn.new
+  def remove_fish_fn
+    RemoveFishFn.new
   end
 end
 
 class Crust < Pizza
-  def remove_anchovy
-    remove_anchovy_fn.for_crust
+  def remove_fish(fish)
+    remove_fish_fn.for_crust(fish)
   end
 end
 
@@ -38,16 +38,29 @@ class Topping < Pizza
     @pizza   = pizza
   end
 
-  def remove_anchovy
-    remove_anchovy_fn.for_topping(topping, pizza)
+  def remove_fish(fish)
+    remove_fish_fn.for_topping(topping, pizza, fish)
   end
 end
 
 class Fish; end
-class Anchovy < Fish; end
-class Salmon < Fish; end
-class Tuna < Fish; end
+class Anchovy < Fish
+  def equal?(object)
+    object.is_a? Anchovy
+  end
+end
+
+class Salmon < Fish
+  def equal?(object)
+    object.is_a? Salmon
+  end
+end
+class Tuna < Fish
+  def equal?(object)
+    object.is_a? Tuna
+  end
+end
 
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
-Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_anchovy
+Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_fish(Salmon.new)
 
