4911557914b0e0fa12f97e626950efdda371e2a8
ipmsteven <steven.lyl147@gmail.com>
2018-04-09 21:45:22 -0700

    add equal for number and  general substitute fn

--- another_pizza.rb
+++ another_pizza.rb
@@ -1,3 +1,17 @@
+class SubstituteFn
+  def for_crust(old_object, new_object)
+    Crust.new
+  end
+
+  def for_topping(topping, pizza, old_object, new_object)
+    if topping.equal? old_object
+      Topping.new(new_object, pizza.substitute_object(old_object, new_object))
+    else
+      Topping.new(topping, pizza.substitute_object(old_object, new_object))
+    end
+  end
+end
+
 class RemoveFn
   def for_crust(object)
     Crust.new
@@ -17,16 +31,28 @@ class Pizza
     raise NotImplementedError
   end
 
+  def substitute_object(old_object, new_object)
+    raise NotImplementedError
+  end
+
   private
 
   def remove_fn
     RemoveFn.new
+  end
+
+  def substitute_fn
+    SubstituteFn.new
   end
 end
 
 class Crust < Pizza
   def remove_object(object)
     remove_fn.for_crust(object)
+  end
+
+  def substitute_object(old_object, new_object)
+    substitute_fn.for_crust(old_object, new_object)
   end
 end
 
@@ -40,6 +66,10 @@ class Topping < Pizza
 
   def remove_object(object)
     remove_fn.for_topping(topping, pizza, object)
+  end
+
+  def substitute_object(old_object, new_object)
+    substitute_fn.for_topping(topping, pizza, old_object, new_object)
   end
 end
 
@@ -61,6 +91,33 @@ class Tuna < Fish
   end
 end
 
+class Number
+end
+
+class Zero < Number
+  def equal?(number)
+    number.is_a? Zero
+  end
+end
+
+class OneMoreThan < Number
+  attr_reader :predecessor
+
+  def initialize(predecessor)
+    @predecessor = predecessor
+  end
+
+  def equal?(number)
+    if number.is_a? OneMoreThan
+      predecessor.equal? number.predecessor
+    else
+      false
+    end
+  end
+end
+
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_object(Salmon.new)
-
+Topping.new(Anchovy.new, Topping.new(Zero.new, Topping.new(Salmon.new, Crust.new))).remove_object(Zero.new)
+Topping.new(OneMoreThan.new(Zero.new), Topping.new(OneMoreThan.new(Zero.new), Topping.new(Salmon.new, Crust.new))).remove_object(Salmon.new)
+Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
