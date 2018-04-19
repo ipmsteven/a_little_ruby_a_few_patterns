5c3917a4109241d09ee305d8b0a54da281f4406a
ipmsteven <steven.lyl147@gmail.com>
2018-04-19 00:49:07 -0700

    bring some props to xx_fn

--- another_pizza.rb
+++ another_pizza.rb
@@ -1,48 +1,63 @@
 class SubstituteFn
-  def for_crust(old_object, new_object)
+  def initialize(old_object, new_object)
+    @old_object = old_object
+    @new_object = new_object
+  end
+
+  def for_crust
     Crust.new
   end
 
-  def for_topping(topping, pizza, old_object, new_object)
+  def for_topping(topping, pizza)
     if topping.equal? old_object
-      Topping.new(new_object, pizza.substitute_object(old_object, new_object, self))
+      Topping.new(new_object, pizza.substitute_object(self))
     else
-      Topping.new(topping, pizza.substitute_object(old_object, new_object, self))
+      Topping.new(topping, pizza.substitute_object(self))
     end
   end
+
+  private
+  attr_reader :old_object, :new_object
 end
 
 class RemoveFn
-  def for_crust(object)
+  def initialize(object)
+    @object = object
+  end
+
+  def for_crust
     Crust.new
   end
 
-  def for_topping(topping, pizza, object)
+  def for_topping(topping, pizza)
     if topping.equal? object
-      pizza.remove_object(object)
+      pizza.remove_object(self)
     else
-      Topping.new(topping, pizza.remove_object(object))
+      Topping.new(topping, pizza.remove_object(self))
     end
   end
+
+  private
+  attr_reader :object
 end
 
 class Pizza
-  def remove_object(object, remove_fn)
+  def remove_object(remove_fn)
     raise NotImplementedError
   end
 
-  def substitute_object(old_object, new_object, substitute_fn)
+  def substitute_object(substitute_fn)
     raise NotImplementedError
   end
 end
 
 class Crust < Pizza
-  def remove_object(object, remove_fn = RemoveFn.new)
-    remove_fn.for_crust(object)
+  def remove_object(remove_fn)
+    remove_fn.for_crust
   end
 
-  def substitute_object(old_object, new_object, substitute_fn = SubstituteFn.new)
-    substitute_fn.for_crust(old_object, new_object)
+  def substitute_object(substitute_fn)
+    substitute_fn.for_crust
   end
 end
 
@@ -54,12 +69,12 @@ class Topping < Pizza
     @pizza   = pizza
   end
 
-  def remove_object(object, remove_fn = RemoveFn.new)
-    remove_fn.for_topping(topping, pizza, object)
+  def remove_object(remove_fn)
+    remove_fn.for_topping(topping, pizza)
   end
 
-  def substitute_object(old_object, new_object, substitute_fn = SubstituteFn.new)
-    substitute_fn.for_topping(topping, pizza, old_object, new_object)
+  def substitute_object(substitute_fn)
+    substitute_fn.for_topping(topping, pizza)
   end
 end
 
@@ -107,7 +122,9 @@ class OneMoreThan < Number
 end
 
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
-Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_object(Salmon.new)
-Topping.new(Anchovy.new, Topping.new(Zero.new, Topping.new(Salmon.new, Crust.new))).remove_object(Zero.new)
-Topping.new(OneMoreThan.new(Zero.new), Topping.new(OneMoreThan.new(Zero.new), Topping.new(Salmon.new, Crust.new))).remove_object(Salmon.new)
+Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new))).remove_object(RemoveFn.new(Salmon.new))
+Topping.new(Anchovy.new, Topping.new(Zero.new, Topping.new(Salmon.new, Crust.new))).remove_object(RemoveFn.new(Zero.new))
+Topping.new(OneMoreThan.new(Zero.new), Topping.new(OneMoreThan.new(Zero.new), Topping.new(Salmon.new, Crust.new))).remove_object(RemoveFn.new(Salmon.new))
 Topping.new(Anchovy.new, Topping.new(Tuna.new, Topping.new(Salmon.new, Crust.new)))
+require 'pry'; binding.pry
+Topping.new(Anchovy.new, Topping.new(Tuna.new, Crust.new)).substitute_object(SubstituteFn.new(Tuna.new, Anchovy.new))
